097fff3323c0a59337f24d1496f81bc10bd2898e
Veniel Dorestave <veniel@noukod.com>
2019-02-01 15:13:09 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,5 +11,10 @@ class UsersController < ApplicationController
     else
       redirect_to :new_user
     end
-end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
 end
